09753d6f1e0709a7890d44af7e863998fb9743bd
Oussama Filani <42185573+filani07@users.noreply.github.com>
2021-06-10 14:10:41 +0100

    yep

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -2,18 +2,33 @@ const btn = document.querySelector("#btn_load")
 const product = document.querySelector("#products")
 const btn_delete = document.querySelector(".btn-add__delete")
 
-const readProduct = async () => {
+const edit_image = document.querySelector("#edit_image")
+const edit_id = document.querySelector('input[name="id"]')
+const edit_title = document.querySelector("#edit_title")
+const edit_price = document.querySelector("#edit_price")
+const edit_category = document.querySelector("#edit_category")
+const edit_description = document.querySelector("#edit_description")
+
+const readProduct = async (withLoader = true, duration = 1000) => {
   product.innerHTML = ''
-  const res = await fetch("http://localhost/ProductApp/Backend/api/product/read.php");
+  
+  const res = await fetch("http://localhost/ProductApp/Backend/api/product/read.php").then(isLoading = false)
   const data = await res.json();
   console.log(data);
+
+  if (withLoader) {
+    product.innerHTML = '<div class="lds-dual-ring"></div>'
+    await new Promise(r => setTimeout(r, duration))
+    product.innerHTML = ''
+  }
   
+
   let output = "";
 
        
         for (let i=0 ; i<data.length; i++) {
           output += 
-          `<div class="col-md-3">
+          /*html*/ `<div class="col-md-3">
           <div class="ibox">
             <div class="ibox-content product-box">
               <div class="product-imitation">
@@ -31,7 +46,7 @@ const readProduct = async () => {
                 <button class="btn btn-xs btn-outline btn-danger btn-card btn-add__delete" onclick="deleteProduct(${data[i].id})">delete <i class="fa fa-trash"></i>
                 </button>
                 <button  class="btn btn-xs btn-outline btn-success btn-card btn-add__edit float-right"   data-toggle="modal"
-                data-target="#edit-modal">edit <i class="fa fa-edit"></i>
+                data-target="#edit-modal" onclick="fetchOne(${data[i].id})">edit <i class="fa fa-edit"></i>
                 </button>
               </div>
               </div>
@@ -60,8 +75,45 @@ fetch('http://localhost/ProductApp/Backend/api/product/delete.php', {
     body: JSON.stringify(obj)
 }).then(res => res.json())
 .then(data => console.log(data))
-.then(readProduct())
+readProduct(true, 250)
 
 }
 
-// btn_delete.addEventListener("click",deleteProduct)
+
+const fetchOne = async (id) => {
+  fetch(`http://localhost/ProductApp/Backend/api/product/read_single.php?id=${id}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      // edit_image.value = data.image
+      edit_id.value = data.id
+      edit_title.value = data.title
+      edit_price.value =data.price
+      edit_category.value = data.category
+      edit_description.value = data.description
+    })
+}
+
+const editProduct = async () => {
+
+  const editModal = document.querySelector('#editForm')
+  const inputs = Array.from(editModal.querySelectorAll('input'))
+
+  let data = {}
+
+  inputs.forEach(input => {
+    data[`${input.name}`] = input.value;
+  })
+
+
+
+  fetch('http://localhost/ProductApp/Backend/api/product/update.php', {
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
